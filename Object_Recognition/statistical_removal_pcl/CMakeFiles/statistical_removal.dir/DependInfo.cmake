4418c2d709145ceae51fb9cda970864ad6096bde
Katsileros <katsileros@gmail.com>
2014-11-25 18:20:38 +0200

    simple-object recognition trials

---
+++ Object_Recognition/statistical_removal_pcl/CMakeFiles/statistical_removal.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/katsi/PANDORA/Object_Recognition/CODE/statistical_removal_pcl/statistical_removal.cpp" "/home/katsi/PANDORA/Object_Recognition/CODE/statistical_removal_pcl/CMakeFiles/statistical_removal.dir/statistical_removal.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "DISABLE_OPENNI"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "DISABLE_OPENNI"
+  "DISABLE_OPENNI"
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "FLANN_STATIC"
+  "qh_QHpointer"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
