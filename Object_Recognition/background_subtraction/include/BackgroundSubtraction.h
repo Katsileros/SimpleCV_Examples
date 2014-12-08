059bd37c12957dda2f8be2a67b0dfca45f6e3789
Katsileros <katsileros@gmail.com>
2014-12-08 21:20:55 +0200

    add some comments

--- Object_Recognition/background_subtraction/include/BackgroundSubtraction.h
+++ Object_Recognition/background_subtraction/include/BackgroundSubtraction.h
@@ -9,33 +9,118 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+@class BackgroundSubtraction
+@brief The main background removal class. Contains the main functionalities of removing the background of an image.
+**/
+
 class BackgroundSubtraction{
 	
-	private:
-		cv::Mat rgb_, depth_;
-		cv::Mat imgThresholded_;
-		cv::Mat srcGray_;	
-	
 	public:
-		BackgroundSubtraction();
-		cv::Mat getRgb(){ return rgb_; }
-		cv::Mat getDepth(){ return depth_; }
-		cv::Mat getImgThresholded(){ return imgThresholded_; }
-		cv::Mat getSrcGray(){ return srcGray_; }
-		void setRgb(cv::Mat y){ rgb_ = y; }
-		void setDepth(cv::Mat y){ depth_ = y; }
-		void setImgThresholded(cv::Mat y){ imgThresholded_ = y; }
-		void setSrcGray(cv::Mat y){ srcGray_ = y; }
-		
-		
+		//!< Variable used to threshold the image
 		int thresh;
+		//!< Variable used to threshold the image
 		int maxThresh;
+		//!< OpenCV item used to create the bounding box
 		cv::RNG rng;
 		
+		//!< Erosion OpenCV function, parameter
+		int erosion_elem;
+		//!< Erosion OpenCV function, parameter
+		int erosion_size;
+		//!< Erosion OpenCV function, parameter
+		int max_elem;
+		//!< Erosion OpenCV function, parameter
+		int max_kernel_size;
+	
+	public:
+		//!< Constructor
+		BackgroundSubtraction();
+		//!<Destructor
+		~BackgroundSubtraction(){};
 		
+		/**
+		@brief Returns the private variable rgb_
+		@return void
+		**/
+		cv::Mat getRgb(){ return rgb_; }
+		
+		/**
+		@brief Returns the private variable depth_
+		@return void
+		**/
+		cv::Mat getDepth(){ return depth_; }
+		
+		/**
+		@brief Returns the private variable imgThresholded_
+		@return void
+		**/
+		cv::Mat getImgThresholded(){ return imgThresholded_; }
+		
+		/**
+		@brief Returns the private variable srcGray_
+		@return void
+		**/
+		cv::Mat getSrcGray(){ return srcGray_; }
+		
+		/**
+		@brief Set the private variable rgb_
+		@return void
+		**/
+		void setRgb( cv::Mat y ){ rgb_ = y; }
+		
+		/**
+		@brief Sets the private variable depth_
+		@return void
+		**/
+		void setDepth( cv::Mat y ){ depth_ = y; }
+		
+		/**
+		@brief Sets the private variable imgThresholded_
+		@return void
+		**/
+		void setImgThresholded( cv::Mat y ){ imgThresholded_ = y; }
+		
+		/**
+		@brief Sets the private variable srcGray_
+		@return void
+		**/
+		void setSrcGray( cv::Mat y ){ srcGray_ = y; }
+		
+		/**
+		@brief Removes the background of an image as good as possible
+			 * First reduces the region of interest using depth map
+			 * After that isolates the item by use the rgb values
+		@return void
+		**/
 		void rmBG();
-		void thresholdImg(cv::Mat img);
-		void threshCallback(int, void* );
+		
+		/**
+		@brief Creates the trackbar with threshold value
+		@return void
+		**/
+		void thresholdImg( cv::Mat img );
+		
+		/**
+		@brief This function calculate the bounding boxes in the given image
+		@param img [cv::Mat] : The input gray scale image
+		@return void
+		**/
+		void threshCallback( int, void* );
+		
+		/**
+		@brief Function: Erosion opencv function 
+		@return void
+		**/
+		void erosion( int, void* );
+		
+	private:
+		//!< Private variables of rgb and depth input images
+		cv::Mat rgb_, depth_;
+		//!< Private variable of thresholded image
+		cv::Mat imgThresholded_;
+		//!< Private variable of the gray image used in callback function
+		cv::Mat srcGray_;
 		
 };
 
