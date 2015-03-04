fac9c60bbed77bf58cd1f2ee98e29252ce23cc08
Katsileros <katsileros@gmail.com>
2015-03-04 20:01:25 +0200

    pose code

---
+++ Object_Recognition/pose_estimation/CMakeFiles/poseEStimation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/poseEStimation.dir/src/poseEstimation.cpp.o"
+  "CMakeFiles/poseEStimation.dir/src/main.cpp.o"
+  "poseEStimation.pdb"
+  "poseEStimation"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/poseEStimation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
